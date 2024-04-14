7d1b7b788832d953c4783a798c9808a38f3bff1a
thecaptaan <thecaptaan@proton.me>
2024-04-15 01:02:29 +0530

    added support for gh-pages

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -12,6 +12,9 @@ import Slay1 from "../assets/slay1.jpeg";
 import Slay2 from "../assets/slay2.jpeg";
 import NaturalTshirt from "../assets/natural__tshirt.png";
 import CategoriesCard from "../Components/CategoriesCard";
+
+import PinkArrow from "../assets/arrow__pink.png";
+import GreenArrow from "../assets/arrow__green.png";
 export default function Index() {
   return (
     <div>
@@ -46,13 +49,16 @@ export default function Index() {
       <div className="papita__pro__container row">
         <div className="papita__items papita__pro__content">
           <div>
-            <div>
+            <div className="papita__pro__inner__content">
               <p className="papita__pro__intro">INTRODUCING</p>
               <h1 className="papita__pro__header">THE PAPITA PRO</h1>
             </div>
             <div>
-              <Link to="">
-                <button>Shop Now</button>
+              <Link className="shop__btn pink__btn" to="">
+                <p className="shop__txt">Shop Now</p>
+                <div className="shop__img">
+                  <img src={PinkArrow} alt="" />
+                </div>
               </Link>
             </div>
           </div>
@@ -76,9 +82,16 @@ export default function Index() {
               alt=""
             />
           </div>
-          <div>
-            <p>Collection</p>
-            <Link to="">Shop Now</Link>
+          <div className="cent">
+            <div className="natural__tshirt__inner__content">
+              <p className="natural__collection">Collection</p>
+              <Link className="shop__btn green__btn" to="">
+                <p className="shop__txt">Shop Now</p>
+                <div className="shop__img">
+                  <img src={GreenArrow} alt="" />
+                </div>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
@@ -94,7 +107,7 @@ export default function Index() {
           <div className="slay__contents">
             Revolutionising style <br /> by empowering <br /> individuals to{" "}
             <br /> embrace their unique <br /> flair and confidently <br />{" "}
-            “Slay the Streets.
+            “Slay the Streets.”
           </div>
         </div>
         <div className="slay__items">
